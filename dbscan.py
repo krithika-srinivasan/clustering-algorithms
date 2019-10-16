97f241ae3a735779ea0e181e8a4e2aaa190d2e4a
Dipack P Panjabi <dipack.panjabi@gmail.com>
2019-10-16 13:54:56 -0400

    Correct DBSCAN algo

--- dbscan.py
+++ dbscan.py
@@ -1,7 +1,7 @@
 import math
 import logging
 import numpy as np
-from sklearn.cluster import DBSCAN
+# from sklearn.cluster import DBSCAN as SKDBSCAN
 from argparse import ArgumentParser
 from util import import_file, rand_score, jaccard_coeff, reduce_dimensionality, plot
 
@@ -11,80 +11,146 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', datefmt='
 def setup_argparser():
     parser = ArgumentParser()
     parser.add_argument("--file", help="File to read data from. Format of the file should be as follows: <index> <ground truth cluster> <point 1> <point 2> ... <point n>", type=str)
-    parser.add_argument("--tolerance", help="Tolerance for centroid shifts", type=float, default=0.1)
+    parser.add_argument("--eps", help="Max distance cut-off", type=float, default=0.1)
     parser.add_argument("--min-points", type=int, help="Min pts", default=3)
     return parser
 
-class Marker:
-    Unvisited = 0
-    Noise = -1
+class Cluster:
+    def __init__(self, name):
+        self.name = name
+        self.points = []
+        self.data = None
+        return
 
-def l2_distance(x, y):
-    """
-    Euclidean distance b/w two points.
-    """
-    if len(x) != len(y):
-        raise ValueError("Dimensions of x {} and y {} are not the same".format(len(x), len(y)))
-    dist = 0.0
-    for idx in range(len(x)):
-        dist += (x[idx] - y[idx]) ** 2
-    return math.sqrt(dist)
+    def add_point(self, point):
+        self.points.append(point)
+        return
 
-def query_region(data, point, eps):
-    neighbours = []
-    for pn in range(0, len(data)):
-        if np.linalg.norm(l2_distance(data[point], data[pn])) < eps:
-            neighbours.append(pn)
-    return neighbours
+    def get_points(self):
+        return self.points
 
-def expand_cluster(data, labels, point, neighbours, cluster_label, min_pts, eps):
-    labels[point] = cluster_label
-    idx = 0
-    while idx < len(neighbours):
-        pn = neighbours[idx]
-        if labels[pn] == Marker.Noise:
-            labels[pn] = cluster_label
-        elif labels[pn] == Marker.Unvisited:
-            labels[pn] = cluster_label
-            pn_neighbours = query_region(data, pn, eps)
-            if len(pn_neighbours) >= min_pts:
-                neighbours = neighbours + pn_neighbours
-        idx += 1
-    return
+    def clear_points(self):
+        self.points = []
+        return
 
-def dbscan(data, min_pts, eps):
-    labels = [Marker.Unvisited] * len(data)
-    curr_cluster_idx = 0
-    for idx, point in enumerate(data):
-        if labels[idx] != Marker.Unvisited:
-            continue
-        neighbours = query_region(data, idx, eps)
-        if len(neighbours) < min_pts:
-            labels[idx] = Marker.Noise
-        else:
-            curr_cluster_idx += 1
-            expand_cluster(data, labels, idx, neighbours, curr_cluster_idx, eps, min_pts)
-    return labels
+    def get_dim(self, dim=0):
+        return [r[dim] for r in self.points]
+
+    def has(self, point):
+        return point in self.points
+
+    def __repr__(self):
+        return "<Cluster name={0}, points={1}>".format(self.name, len(self.points))
+
+NOISE_LABEL = -1
+
+class DBSCAN:
+    def __init__(self, eps, min_points):
+        self.eps = eps
+        self.min_points = min_points
+        self.clusters = set()
+        self.visited = []
+        self.labels = []
+        return
+
+    def reset_params(self):
+        self.clusters = set()
+        self.visited = []
+        self.labels = []
+        return
+
+    def l2_distance(self, x, y):
+        """
+        Euclidean distance b/w two points.
+        """
+        if len(x) != len(y):
+            raise ValueError("Dimensions of x {} and y {} are not the same".format(len(x), len(y)))
+        dist = 0.0
+        for idx in range(len(x)):
+            dist += (x[idx] - y[idx]) ** 2
+        return math.sqrt(dist)
+
+    def expand_cluster(self, cluster, point, neighbours, neighbour_indexes):
+        indexes = []
+        cluster.add_point(point)
+        for pn_idx, pn in enumerate(neighbours):
+            if pn not in self.visited:
+                self.visited.append(pn)
+                pn_neighbours, pn_neighbour_indexes = self.query_region(pn)
+                if len(pn_neighbours) >= self.min_points:
+                    for nnp_idx, nnp in enumerate(pn_neighbours):
+                        if nnp not in neighbours:
+                            neighbours.append(nnp)
+                            neighbour_indexes.append(pn_neighbour_indexes[nnp_idx])
+                for other_cluster in self.clusters:
+                    if not other_cluster.has(pn):
+                        if not cluster.has(pn):
+                            cluster.add_point(pn)
+                            indexes.append(neighbour_indexes[pn_idx])
+                if len(self.clusters) == 0:
+                    if not cluster.has(pn):
+                        cluster.add_point(pn)
+                        indexes.append(neighbour_indexes[pn_idx])
+        self.clusters.add(cluster)
+        return indexes
+
+    def query_region(self, point):
+        result = []
+        indexes = []
+        for didx, dpoint in enumerate(self.data):
+            if dpoint != point:
+                if self.l2_distance(dpoint, point) <= self.eps:
+                    result.append(dpoint)
+                    indexes.append(didx)
+        return result, indexes
+
+    def dbscan(self, data):
+        self.reset_params()
+        self.data = data
+        self.labels = [-1] * len(data)
+
+        noise_cluster = Cluster('Noise')
+        self.clusters.add(noise_cluster)
+
+        count = 0
+        for pidx, point in enumerate(data):
+            if point not in self.visited:
+                count += 1
+                self.visited.append(point)
+                neighbours, neighbour_indexes = self.query_region(point)
+                if len(neighbours) < self.min_points:
+                    noise_cluster.add_point(point)
+                    self.labels[pidx] = NOISE_LABEL
+                else:
+                    cluster_idx = len(self.clusters)
+                    name = "cluster-{0}".format(cluster_idx)
+                    new_cluster = Cluster(name)
+                    indexes = self.expand_cluster(new_cluster, point, neighbours, neighbour_indexes)
+                    self.labels[pidx] = cluster_idx
+                    for added_idx in indexes:
+                        self.labels[added_idx] = cluster_idx
+        return
 
 def main():
     args = setup_argparser().parse_args()
     filepath = args.file
     min_pts = args.min_points
-    tolerance = args.tolerance
+    eps = args.eps
 
     logging.info(args)
 
     data, truth_clusters = import_file(filepath, correct_clusters=False)
 
-    labels = dbscan(data, min_pts, tolerance)
-    logging.info("Rand index: {}".format(rand_score(truth_clusters, labels)))
-    logging.info("Jaccard coeff: {}".format(jaccard_coeff(truth_clusters, labels)))
-
-    skdb = DBSCAN(eps=tolerance, min_samples=min_pts).fit(data)
-    print(skdb.labels_)
-    print(labels)
-
-    # plot(reduce_dimensionality(data), labels, None, suffix="dbscan")
+    db = DBSCAN(eps=eps, min_points=min_pts)
+    db.dbscan(data)
+    print(db.clusters)
+    print(db.labels)
+    # skdb = SKDBSCAN(eps=eps, min_samples=min_pts, algorithm="brute").fit(data)
+    print(np.unique(db.labels, return_counts=True))
+    # print(np.unique(skdb.labels_, return_counts=True))
+    # There's barely any difference b/w what we classify and what Sklearns does - this looks correct
+    plot(reduce_dimensionality(data), db.labels, None, suffix="dbscan")
+    # plot(reduce_dimensionality(data), skdb.labels_, None, suffix="skdbscan")
     return
 
 if __name__ == "__main__":
